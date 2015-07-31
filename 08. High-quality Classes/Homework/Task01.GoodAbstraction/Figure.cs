692e4dd13f4df3fdee37fcb70bc6efea34ccb303
madmarks <bratmiro@gmail.com>
2015-07-31 20:53:29 +0300

    08. High-Quality Classes - HomeWork

---
+++ 08. High-quality Classes/Homework/Task01.GoodAbstraction/Figure.cs
@@ -0,0 +1,27 @@
+﻿namespace Task01.GoodAbstraction
+{
+    using System.Text;
+
+    internal abstract class Figure
+    {
+        internal Figure()
+        {
+        }
+
+        internal abstract double CalcPerimeter();
+
+        internal abstract double CalcSurface();
+
+        public override string ToString()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine("I am a " + this.GetType().Name);
+            result.AppendFormat("My perimeter is {0:f2}", this.CalcPerimeter());
+            result.AppendLine();
+            result.AppendFormat("My surface is {0:f2}.", this.CalcSurface());
+
+            return result.ToString();
+        }
+    }
+}
---
+++ 08. High-quality Classes/Homework/Task01.GoodAbstraction/FiguresExample.cs
@@ -0,0 +1,18 @@
+﻿namespace Task01.GoodAbstraction
+{
+    using System;
+
+    public class FiguresExample
+    {
+        public static void Main()
+        {
+            Circle circle = new Circle(5);
+
+            Console.WriteLine(circle.ToString());
+
+            Rectangle rect = new Rectangle(2, 3);
+
+            Console.WriteLine(rect.ToString());
+        }
+    }
+}
